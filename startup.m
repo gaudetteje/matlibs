eb5a792b5a3a60209ceb4d8b9c390e3b1d01042f
Jason Gaudette <jason_gaudette@brown.edu>
2018-07-09 17:29:41 -0400

    added startup.m and example

---
+++ startup.m
@@ -0,0 +1,11 @@
+% MATLAB startup file to include libraries and modify environment
+%   Place this file in the startup directory specified by the Matlab
+%   shortcut's properties
+%   
+format compact
+
+display(sprintf('Loading user preferences in "%s"...\n', mfilename('fullpath')))
+
+%addpath(genpath('/Users/jasongaudette/src/matlibs'))
+
+
